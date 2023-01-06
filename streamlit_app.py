c27f131eed24ce6a993cea9acfd3ed9db7181c6b
snowflakeliz23 <122120870+snowflakeliz23@users.noreply.github.com>
2023-01-06 14:49:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My Parent's New Healthy Diner")
+streamlit.header("Breakfast Mekbib")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard- Boiled free range eggs")
