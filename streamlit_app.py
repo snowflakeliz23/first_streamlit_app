ac7ac2bc499959cc804b708df5621f0e43a69c1b
snowflakeliz23 <122120870+snowflakeliz23@users.noreply.github.com>
2023-01-06 14:53:09 -0500

    Update streamlit_app.py
    Import csv file into pandas data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,4 @@ streamlit.text("Hard- Boiled free range eggs")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
