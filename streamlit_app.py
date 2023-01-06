85e8bbbce11e70517fe4fe8ea4296b9fc7ffc879
snowflakeliz23 <122120870+snowflakeliz23@users.noreply.github.com>
2023-01-06 14:52:33 -0500

    Update streamlit_app.py
    Imported Pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.text("Omega 3 & Blueberry Oatmeal")
 streamlit.text("Kale, Spinach & Rocket Smoothie")
 streamlit.text("Hard- Boiled free range eggs")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
